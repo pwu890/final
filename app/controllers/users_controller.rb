5002be7872121a8c9e245d666d9b41cbaa5a93be
vagrant <vagrant@kellogg-dev-box>
2014-06-10 00:10:03 +0000

    lets go

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find_by("id" => params["id"])
+
+    logger.debug @user["id"].inspect
+    logger.debug cookies["user_id"].inspect
+
+    if (@user["id"] != cookies["user_id"].to_i)
+      redirect_to "/"
+    end
+
+  end
+
+  def create
+    existing_user = User.find_by("username" => params["username"])
+
+    if existing_user == nil
+      User.create("username" => params["username"],
+                  "password" => params["password"],
+                  "name" => params["name"])
+
+      redirect_to "/restaurants"
+    else
+      @message = "That username is taken. Try an alternative."
+      render "new"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,31 @@ Final::Application.routes.draw do
   # Home page
   get "/" => "index#index"
 
+
+  # Sign in/out
+  get "/logout" => 'sessions#logout'
+  get "/login" => 'sessions#login'
+  post "/authenticate" => 'sessions#authenticate'
+
+
+  # Resource: Users
+
+  # --- Create
+  get "/users/new" => 'users#new'
+  post "/users" => 'users#create'
+
+  # --- Read
+  get "/users" => 'users#index'
+  get "/users/:id" => 'users#show'
+
+  # -- Update
+  get "/users/:id/edit" => 'users#edit'
+  patch "/users/:id" => 'users#update'
+
+  # --- Delete
+  delete "/users/:id" => 'users#destroy'
+
+
   # Resource: Restaurants
 
   # -- Create
